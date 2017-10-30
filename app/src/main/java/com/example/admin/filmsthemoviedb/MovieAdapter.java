022f11d0ea28283d3aefe7b8b4c5b5fb39e41804
Ayur <gangrue@gmail.com>
2017-10-30 15:55:30 +0800

    add poster

--- app/src/main/java/com/example/admin/filmsthemoviedb/MovieAdapter.java
+++ app/src/main/java/com/example/admin/filmsthemoviedb/MovieAdapter.java
@@ -3,6 +3,7 @@ package com.example.admin.filmsthemoviedb;
 
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.admin.filmsthemoviedb.api.MovieResponse;
 import com.example.admin.filmsthemoviedb.api.MovieResponseBody;
 
@@ -32,12 +34,18 @@ class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
         return new ViewHolder(view);
     }
 
+    public static final String IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mEtListItemTitle.setOnClickListener(view -> {
-
-        });
+        Glide
+                .with(holder.itemView)
+                .load( IMAGE_URL + mMovie.getmResults().get(position).getmPosterPath())
+                .into(holder.mIvListItemPoster);
         holder.mEtListItemTitle.setText(mMovie.getmResults().get(position).getmTitle());
+        holder.itemView.setOnClickListener(view -> {
+            Log.d("DEBUG", "click on " + position);
+        });
     }
 
     @Override
@@ -59,7 +67,6 @@ class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
         public ViewHolder(View itemView) {
             super(itemView);
-
             ButterKnife.bind(this, itemView);
         }
     }
