6fe70efc40d2207cc4a7c5f3a3ceb96ebcb330d9
Ayur <gangrue@gmail.com>
2017-11-03 22:30:43 +0800

    add realm dependencies

--- app/src/main/java/com/example/admin/filmsthemoviedb/App.java
+++ app/src/main/java/com/example/admin/filmsthemoviedb/App.java
@@ -4,6 +4,10 @@ package com.example.admin.filmsthemoviedb;
 import android.app.Application;
 import android.content.Context;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.rx.RealmObservableFactory;
+
 public class App extends Application {
 
     private static Context mContext;
@@ -12,7 +16,16 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = this;
-        //Realm.init(this);
+
+        initializeRealm();
+    }
+
+    private void initializeRealm() {
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .rxFactory(new RealmObservableFactory())
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 
     public static Context getContext() {
